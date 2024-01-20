794454dec1739c08700231ffd572af0a546d6974
Yakuphan Bilmez <yakuphan95@gmail.com>
2024-01-20 21:10:15 +0300

    Sea
    sara

---
+++ 01_perfect_number/perfect_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    printf("Enter Number : ");
+    scanf("%d", &num);
+
+    int i=0;
+    int count = 0;
+    for(i=1; i<num; i++){
+        if( num % i == 0) {
+            count += i;
+        }
+    }
+
+    if( count == num ) {
+        printf("Perfect Number\n");
+    } else {
+        printf("Not Perfect Number\n");
+    }
+
+    return 0;
+}
