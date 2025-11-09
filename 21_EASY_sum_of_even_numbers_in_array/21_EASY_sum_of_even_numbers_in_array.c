0bcad2c8bf69a613f48c7fafe51f5c87b38bc35a
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2025-11-09 06:26:57 +0000

    Create solution for 21_EASY_sum_of_even_numbers_in_array

---
+++ 21_EASY_sum_of_even_numbers_in_array/21_EASY_sum_of_even_numbers_in_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {10, 3, 8, 15, 6, 7, 2, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum_of_evens = 0;
+    int i;
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            sum_of_evens += arr[i];
+        }
+    }
+    printf("Sum of even numbers: %d\n", sum_of_evens);
+    return 0;
+}
