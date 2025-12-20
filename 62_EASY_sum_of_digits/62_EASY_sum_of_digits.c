5e81c1593873b622817157ba630a90dc78abc527
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2025-12-20 06:30:35 +0000

    Add EASY problem: sum_of_digits

---
+++ 62_EASY_sum_of_digits/62_EASY_sum_of_digits.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int number;
+    int sum = 0;
+    scanf("%d", &number);
+    while (number > 0) {
+        sum += number % 10;
+        number /= 10;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
