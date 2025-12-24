cfc09a2f931ef622ae86b6dcdab0a0051519b486
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2025-12-24 06:34:22 +0000

    Yeni C alıştırması (EASY) eklendi: sum_array_elements

---
+++ 66_EASY_sum_array_elements/66_EASY_sum_array_elements.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    printf("%d\n", sum);
+    return 0;
+}
