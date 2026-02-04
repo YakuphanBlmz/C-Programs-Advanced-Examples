9d1340d9c2ee3dd32c363045ef8e86857a04922d
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-02-04 06:53:06 +0000

    Add sum_even_numbers_in_array challenge

---
+++ 100_EASY_sum_even_numbers_in_array/100_EASY_sum_even_numbers_in_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum_even = 0;
+    int i;
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            sum_even += arr[i];
+        }
+    }
+    printf("Sum of even numbers: %d\n", sum_even);
+    return 0;
+}
