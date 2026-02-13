31d40bc1a4a61698700adfe977a3af1443225963
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-02-13 06:59:37 +0000

    Yeni C alıştırması (EASY) eklendi: sum_of_even_numbers_in_array

---
+++ 100_EASY_sum_of_even_numbers_in_array/100_EASY_sum_of_even_numbers_in_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+    int i;
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            sum += arr[i];
+        }
+    }
+    printf("The sum of even numbers is: %d\n", sum);
+    return 0;
+}
