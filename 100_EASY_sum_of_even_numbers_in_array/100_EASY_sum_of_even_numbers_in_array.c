f300b7bd42bc057d21031ca3d9699af9a01035a6
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-02-26 07:00:31 +0000

    Add exercise 100 (EASY)

--- 100_EASY_sum_of_even_numbers_in_array/100_EASY_sum_of_even_numbers_in_array.c
+++ 100_EASY_sum_of_even_numbers_in_array/100_EASY_sum_of_even_numbers_in_array.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {10, 25, 30, 41, 52, 63, 70};
+    int arr[] = {10, 23, 14, 7, 30, 11, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int sum_even = 0;
+    int sum = 0;
     int i;
     for (i = 0; i < n; i++) {
         if (arr[i] % 2 == 0) {
-            sum_even += arr[i];
+            sum += arr[i];
         }
     }
-    printf("Sum of even numbers: %d\n", sum_even);
+    printf("Dizideki çift sayıların toplamı: %d\n", sum);
     return 0;
 }
