3367ed0424e78793cb8675e3d4e34627ef049847
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-03-13 06:54:10 +0000

    Yeni C alıştırması (EASY) eklendi: find_maximum_in_array

---
+++ 100_EASY_find_maximum_in_array/100_EASY_find_maximum_in_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int max_val = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+    printf("%d\n", max_val);
+    return 0;
+}
