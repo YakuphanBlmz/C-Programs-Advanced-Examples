19bce494512f4e668706e11573a6cf963d6681cc
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-04-08 07:19:56 +0000

    Yeni C alıştırması (EASY) eklendi: sum_of_array_elements

--- 100_EASY_sum_of_array_elements/100_EASY_sum_of_array_elements.c
+++ 100_EASY_sum_of_array_elements/100_EASY_sum_of_array_elements.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main() {
-    int numbers[] = {10, 25, 5, 30, 15};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int arr[5];
     int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += numbers[i];
+    int i;
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
     }
-    printf("Array elements sum: %d\n", sum);
+    for (i = 0; i < 5; i++) {
+        sum = sum + arr[i];
+    }
+    printf("%d\n", sum);
     return 0;
 }
