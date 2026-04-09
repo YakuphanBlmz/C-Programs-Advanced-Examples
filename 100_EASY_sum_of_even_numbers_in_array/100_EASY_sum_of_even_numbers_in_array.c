af64ddb1106155bc075d13d0edcdcae525c862a8
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-04-09 07:22:49 +0000

    Add EASY problem: sum_of_even_numbers_in_array

--- 100_EASY_sum_of_even_numbers_in_array/100_EASY_sum_of_even_numbers_in_array.c
+++ 100_EASY_sum_of_even_numbers_in_array/100_EASY_sum_of_even_numbers_in_array.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[] = {10, 5, 8, 12, 3, 7, 6, 15, 20};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int sum = 0;
+    int sum_even = 0;
     int i;
     for (i = 0; i < n; i++) {
         if (arr[i] % 2 == 0) {
-            sum = sum + arr[i];
+            sum_even += arr[i];
         }
     }
-    printf("Dizideki cift sayilarin toplami: %d\n", sum);
+    printf("Sum of even numbers: %d\n", sum_even);
     return 0;
 }
