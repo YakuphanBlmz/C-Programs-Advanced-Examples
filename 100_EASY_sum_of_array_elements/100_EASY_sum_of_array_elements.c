0c1ae85056151f6b875ea3bac3f18faed9fb5ed5
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-06-15 12:12:49 +0000

    Create solution for 100_EASY_sum_of_array_elements

--- 100_EASY_sum_of_array_elements/100_EASY_sum_of_array_elements.c
+++ 100_EASY_sum_of_array_elements/100_EASY_sum_of_array_elements.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[5] = {10, 25, 30, 45, 50};
     int sum = 0;
     int i;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < 5; i++) {
         sum = sum + arr[i];
     }
-    printf("Sum of array elements: %d\n", sum);
+    printf("The sum of array elements is: %d\n", sum);
     return 0;
 }
