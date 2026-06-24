ceafdd05204f2f26fd6dd82bc7de74957c8be707
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-06-24 09:28:35 +0000

    Add exercise 100 (EASY)

--- 100_EASY_count_positive_numbers_in_array/100_EASY_count_positive_numbers_in_array.c
+++ 100_EASY_count_positive_numbers_in_array/100_EASY_count_positive_numbers_in_array.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {10, -5, 0, 7, -2, 3, -8, 1};
+    int arr[] = {1, -2, 3, 0, 5, -1, 7, 10, -5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int positiveCount = 0;
-    for (int i = 0; i < n; i++) {
+    int count = 0;
+    int i;
+    for (i = 0; i < n; i++) {
         if (arr[i] > 0) {
-            positiveCount++;
+            count++;
         }
     }
-    printf("Dizideki pozitif sayilarin adedi: %d\n", positiveCount);
+    printf("Pozitif sayilarin sayisi: %d\n", count);
     return 0;
 }
