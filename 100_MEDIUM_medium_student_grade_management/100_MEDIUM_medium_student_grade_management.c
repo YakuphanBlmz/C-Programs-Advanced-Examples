d9d508dcda97f58546fbc435c5efd6194f5d9658
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-07-28 08:33:03 +0000

    Add MEDIUM problem: medium_student_grade_management

--- 100_MEDIUM_medium_student_grade_management/100_MEDIUM_medium_student_grade_management.c
+++ 100_MEDIUM_medium_student_grade_management/100_MEDIUM_medium_student_grade_management.c
@@ -1,58 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 typedef struct {
     int id;
     char name[50];
-    int grades[3];
+    float exam1;
+    float exam2;
+    float final_grade;
 } Student;
-double calculate_average(Student s) {
-    int sum = 0;
-    for (int i = 0; i < 3; i++) {
-        sum += s.grades[i];
-    }
-    return (double)sum / 3.0;
-}
 int main() {
-    const int MAX_STUDENTS = 3; 
-    Student students[MAX_STUDENTS]; 
-    students[0].id = 101;
-    strcpy(students[0].name, "Ali Veli");
-    students[0].grades[0] = 85;
-    students[0].grades[1] = 90;
-    students[0].grades[2] = 78;
-    students[1].id = 102;
-    strcpy(students[1].name, "Ayse Fatma");
-    students[1].grades[0] = 92;
-    students[1].grades[1] = 88;
-    students[1].grades[2] = 95;
-    students[2].id = 103;
-    strcpy(students[2].name, "Mehmet Can");
-    students[2].grades[0] = 70;
-    students[2].grades[1] = 75;
-    students[2].grades[2] = 80;
-    double max_average = -1.0;
+    Student students[3];
+    int i;
+    printf("Lutfen 3 ogrencinin bilgilerini giriniz:\n");
+    for (i = 0; i < 3; i++) {
+        printf("Ogrenci %d ID: ", i + 1);
+        scanf("%d", &students[i].id);
+        printf("Ogrenci %d Adi: ", i + 1);
+        scanf("%s", students[i].name);
+        printf("Ogrenci %d Sinav 1 Notu: ", i + 1);
+        scanf("%f", &students[i].exam1);
+        printf("Ogrenci %d Sinav 2 Notu: ", i + 1);
+        scanf("%f", &students[i].exam2);
+        students[i].final_grade = (students[i].exam1 + students[i].exam2) / 2.0;
+        printf("\n");
+    }
+    printf("\n--- Tum Ogrenci Bilgileri ---\n");
+    printf("ID\tAd\tSinav1\tSinav2\tFinal Notu\n");
+    for (i = 0; i < 3; i++) {
+        printf("%d\t%s\t%.2f\t%.2f\t%.2f\n",
+               students[i].id,
+               students[i].name,
+               students[i].exam1,
+               students[i].exam2,
+               students[i].final_grade);
+    }
+    float max_grade = -1.0;
     int top_student_index = -1;
-    printf("--- Student Grades and Averages ---\n");
-    for (int i = 0; i < MAX_STUDENTS; i++) {
-        double current_average = calculate_average(students[i]);
-        printf("ID: %d, Name: %s, Grades: %d, %d, %d, Average: %.2f\n",
-               students[i].id, students[i].name, 
-               students[i].grades[0], students[i].grades[1], students[i].grades[2],
-               current_average);
-        if (current_average > max_average) {
-            max_average = current_average;
+    for (i = 0; i < 3; i++) {
+        if (students[i].final_grade > max_grade) {
+            max_grade = students[i].final_grade;
             top_student_index = i;
         }
     }
+    printf("\n--- En Yuksek Final Notu Alan Ogrenci ---\n");
     if (top_student_index != -1) {
-        printf("\n--- Student with Highest Average ---\n");
-        printf("ID: %d, Name: %s, Average: %.2f\n",
-               students[top_student_index].id,
-               students[top_student_index].name,
-               max_average);
+        printf("ID: %d\n", students[top_student_index].id);
+        printf("Ad: %s\n", students[top_student_index].name);
+        printf("Sinav 1 Notu: %.2f\n", students[top_student_index].exam1);
+        printf("Sinav 2 Notu: %.2f\n", students[top_student_index].exam2);
+        printf("Final Notu: %.2f\n", students[top_student_index].final_grade);
     } else {
-        printf("\nNo student data processed.\n");
+        printf("Ogrenci bulunamadi veya notlar gecersiz.\n");
     }
     return 0;
 }
