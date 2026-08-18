3797666ec93610cb38d69dd0dd26fed56e42f369
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-08-18 06:40:32 +0000

    Add exercise 100 (EASY)

--- 100_EASY_sum_of_array_elements/100_EASY_sum_of_array_elements.c
+++ 100_EASY_sum_of_array_elements/100_EASY_sum_of_array_elements.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {5, 12, 8, 20, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[5] = {10, 25, 15, 30, 5};
     int sum = 0;
     int i;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < 5; i++) {
         sum = sum + arr[i];
     }
-    printf("Dizideki elemanlarin toplami: %d\n", sum);
+    printf("Dizinin elemanlari toplami: %d\n", sum);
     return 0;
 }
