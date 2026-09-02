046d0bec595bf64468a5f4099e129980e5a9eb32
YakuphanBlmz <YAKUPHANIN-GITHUB-EMAILI>
2026-09-02 10:43:13 +0000

    Yeni C alıştırması (EASY) eklendi: count_even_numbers_in_array

--- 100_EASY_count_even_numbers_in_array/100_EASY_count_even_numbers_in_array.c
+++ 100_EASY_count_even_numbers_in_array/100_EASY_count_even_numbers_in_array.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {10, 5, 22, 13, 7, 30, 8};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int numbers[] = {10, 5, 22, 13, 7, 30, 11, 4};
+    int n = sizeof(numbers) / sizeof(numbers[0]);
     int even_count = 0;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 == 0) {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (numbers[i] % 2 == 0) {
             even_count++;
         }
     }
-    printf("Dizideki cift sayi adedi: %d\n", even_count);
+    printf("Number of even integers: %d\n", even_count);
     return 0;
 }
